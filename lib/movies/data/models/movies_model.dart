51739b6339bc1e82c0d12c63f6f767342e9d930f
Ali Abdelmoaty <68239507+aliabdelmoaty@users.noreply.github.com>
2023-11-07 19:53:36 +0200

    create movie model in data

---
+++ lib/movies/data/models/movies_model.dart
@@ -0,0 +1,23 @@
+import 'package:movie/movies/domain/entities/movie.dart';
+
+class MovieModel extends Movie {
+  const MovieModel(
+      {required super.id,
+      required super.adult,
+      required super.title,
+      required super.backdropPath,
+      required super.genreIds,
+      required super.overview,
+      required super.releaseDate,
+      required super.voteAverage});
+
+  factory MovieModel.fromJson(Map<String, dynamic> json) => MovieModel(
+      id: json['id'],
+      adult: json['adult'],
+      title: json['title'],
+      backdropPath: json['backdrop_path'],
+      genreIds: List<int>.from(json['genre_ids'].map((e) => e)),
+      overview: json['overview'],
+      releaseDate: json['release_date'],
+      voteAverage: json['vote_average']);
+}
